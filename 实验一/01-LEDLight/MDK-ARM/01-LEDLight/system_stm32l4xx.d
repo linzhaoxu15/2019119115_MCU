3b56de138f4d6af48857bbdb351bdd056bc128be
LAPTOP-M52EL755\小尾巴 <1095486646@qq.com>
2021-11-29 11:11:13 +0800

    no message

--- 实验一/01-LEDLight/MDK-ARM/01-LEDLight/system_stm32l4xx.d
+++ 实验一/01-LEDLight/MDK-ARM/01-LEDLight/system_stm32l4xx.d
@@ -2,7 +2,7 @@
 01-ledlight\system_stm32l4xx.o: ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l431xx.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/CMSIS/Include/core_cm4.h
-01-ledlight\system_stm32l4xx.o: E:\keil\ARM\ARMCC\Bin\..\include\stdint.h
+01-ledlight\system_stm32l4xx.o: D:\大学课程\单片机\工具\keil\ARM\ARMCC\Bin\..\include\stdint.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/CMSIS/Include/cmsis_version.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
@@ -14,7 +14,7 @@
 01-ledlight\system_stm32l4xx.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-01-ledlight\system_stm32l4xx.o: E:\keil\ARM\ARMCC\Bin\..\include\stddef.h
+01-ledlight\system_stm32l4xx.o: D:\大学课程\单片机\工具\keil\ARM\ARMCC\Bin\..\include\stddef.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h
 01-ledlight\system_stm32l4xx.o: ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h
