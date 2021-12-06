f4b556bb782395fa5600f2d6d18372b323619772
LAPTOP-M52EL755\小尾巴 <1095486646@qq.com>
2021-12-06 10:16:29 +0800

    实验一

--- 实验一/02-LEDflashing/Core/Src/main.c
+++ 实验一/02-LEDflashing/Core/Src/main.c
@@ -97,14 +97,14 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 		//第一种闪烁方式
-		//HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);//LED亮灭状态翻转
-		//HAL_Delay(1000);//延时1000毫秒=1秒钟
+		HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);//LED亮灭状态翻转
+		HAL_Delay(1000);//延时1000毫秒=1秒钟
 		
 		//第二种闪烁方式，亮灭时间不一样
-		HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_SET);//LED亮
-		HAL_Delay(1500);//延时1000毫秒=1秒钟
-		HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_RESET);//LED灭
-		HAL_Delay(200);//延时200毫秒=0.2秒钟
+		//HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_SET);//LED亮
+		//HAL_Delay(1500);//延时1000毫秒=1秒钟
+		//HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_RESET);//LED灭
+		//HAL_Delay(200);//延时200毫秒=0.2秒钟
   }
   /* USER CODE END 3 */
 }
